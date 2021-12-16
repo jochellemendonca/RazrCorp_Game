6089065a0fafbc44d17efa8faf0573d7da78fd4a
jmendonca <jmendonca@controlcase.com>
2021-12-16 18:48:08 +0530

    Removing few lines

--- tests/Feature/BeginGameCommandTest.php
+++ tests/Feature/BeginGameCommandTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Artisan;
-use Mockery as m;
+
 
 class BeginGameCommandTest extends TestCase
 {
